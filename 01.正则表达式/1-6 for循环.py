64535945d46735bbc6fba4cd02b4497718f7db71
724598042@qq.com <724598042@qq.com>
2018-02-24 13:58:58 +0800

    V型

--- 01.正则表达式/1-6 for循环.py
+++ 01.正则表达式/1-6 for循环.py
@@ -1,5 +1,30 @@
 # for循环
+
 for i in range(1,5):
     print(i)
 else:
     print("The for loop is over")
+
+
+# break语句
+
+while True:
+    s=input('Enter something:')
+    print(s)
+    if s=='quit':
+        print("input value is {0}".format(s))
+        break
+    print('Length of the string is {0}'.format(len(s)))
+print('Done')
+
+
+# continue语句
+
+while True:
+    s=input("Enter something:")
+    if s=='quit':
+        break
+    if len(s)<3:
+        print('Too Small')
+        continue
+    print('Input is of sufficient length')
