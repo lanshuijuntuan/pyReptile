bfa7e391bc684a8893a0fc735dca80eba1041dbc
724598042@qq.com <724598042@qq.com>
2018-02-24 09:53:20 +0800

    cv

--- 01.正则表达式/1-1.first_helllo.py
+++ 01.正则表达式/1-1.first_helllo.py
@@ -28,5 +28,65 @@ print "dsfsfsdf \
 sdfsfsfs";
 
 
+# 字符串格式化
+age=28
+name="Swaroop"
+
+print("{0} was {1} years old when he wrote this book".format(name,age))
+
+print("Why is {0} playing with that python".format(name))
+
+
+print("{} was {} years old when he wrote this book".format(name,age))
+
+print("Why is {} playing  with that python".format(name))
+
+# 对于浮点数“0.333”保留小数点（.）后三位
+print('{0:.3f}'.format(1.0/3))
+
+# 使用下划线填充文本，并保持文字处于中间位置
+# 使用（^）定义"_____hello____"字符串长度为11
+print('{0:_^11}'.format('hello'))
+
+
+# 注意 print 总是会以一个不可见的'新一行'字符（\n)结尾，
+# 因此重复调用 print 将会在相互独立的一行中分别打印。
+# 为防止打印过程中出现这一换行符，你可以通过 end 指定其结尾
+
+# print('a',end='')  不支持
+
+# print('b',end='')  不支持
+
+
+# 转义序列
+
+print('this is the first line \n this is the second line')
+
+# 如果不需要转义请在字符串前面加r或者R来指定一个原始（Raw）字符串：
+
+print(r"Newlines are indicated by \n")
+
+
+# 使用变量与字面量
+i=5
+print(i)
+i=i+1
+print(i)
+
+s='''This is a multi-line string.
+This is the second line.'''
+
+print(s)
+
+
+# python是一种缩进严格的语言
+
+
+# 以下缩进不严格会报错
+# i=5
+#   print（“Value is ”,i)
+# print("I repeat,the value is",i)
+
+
 
 
---
+++ 01.正则表达式/1-3.运算符和表达式.py
@@ -0,0 +1,119 @@
+# 运算符和表达式
+
+# 加法运算
+a=12
+b=18
+c=a+b
+print(c)
+
+
+# 减法运算
+
+a=12
+b=15
+c=a-b
+print(c)
+
+# 乘法运算
+
+a=12
+b=22
+c=a*b
+print(c)
+
+# 除法运算
+
+a=12
+b=3
+c=a/b
+print(c)
+
+# 乘方运算
+
+a=12
+b=3
+c=a**b
+print(c)
+
+# 整除
+
+a=13
+b=3
+c=a//b
+print(c)
+
+a=-13
+c=a//b
+print(c)
+
+# 取模
+
+a=13
+b=3
+c=a%b
+print(c)
+
+a=-25.5
+b=2.25
+c=a%b
+print(c)
+
+
+# 左移
+
+a=8
+b=a<<2
+print(b)
+
+# 右移
+
+a=11
+b=a>>1
+print(b)
+
+# 按位与
+
+a=5
+b=3
+c=a&b
+print(c)
+
+# 按位或
+a=5
+b=3
+c=a|b
+print(c)
+
+
+# 按位异或
+
+a=5
+b=3
+c=a^b
+print(c)
+
+
+
+# 按位取反
+
+a=5
+b=~a
+print(b)
+
+# 小于
+
+# 大于
+
+# 等于
+
+# 大于等于
+
+# 小于等于
+
+# 不等于
+
+# not（布尔“非”）
+
+# or (布尔“或”)
+
+# and (布尔“与”)
